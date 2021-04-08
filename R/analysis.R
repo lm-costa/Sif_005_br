40217fa81d9429ef5797c69d01f2986e0f35cf26
lm-costa <luism.costa00@gmail.com>
2021-04-08 15:27:52 -0300

    teste

--- R/analysis.R
+++ R/analysis.R
@@ -9,3 +9,12 @@ sif %>%
   ggplot(aes(x=Lon,y=Lat)) +
   geom_point()
   
+# usethis::use_git_config(
+#   user.name= "lm-costa", 
+#   user.email="luism.costa00@gmail.com"
+# )
+# 
+# usethis::create_github_token()
+# gitcreds::gitcreds_set(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)
+
+#comentario 
